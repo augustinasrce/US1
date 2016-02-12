2171a3ef95e6f657e4f61d733107d0fec860419b
Augustinas <augustinas.sueris@gmail.com>
2016-02-12 23:25:53 +0200

    Homescreen done!

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,14 @@
     var app = angular.module('HomeScreen', []);
 
 
-    app.controller('SimpleCtrl', function($scope) {
-        $scope.title = "Susisiekite su mumis";
-        $scope.register = "Registruokitės konsultacijai internetu";
-        $scope.contacts = "Parašykite mums";
+    app.controller('SimpleCtrl', function($scope, $http) {
+    $http({
+      method: 'GET',
+      url: '/lt'
+    }).then(function successCallback(response) {
+        $scope.title = response.data.homeScreenNameList[0];
+        $scope.register = response.data.homeScreenNameList[1];
+        $scope.contacts = response.data.homeScreenNameList[2];
         document.title = $scope.title;
+      });
     });
