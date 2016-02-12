252a42b5ca025d45f8901fde5f338b09d33fd8ef
p998ueh <augustinas.sueris@gmail.com>
2016-02-12 17:33:43 +0200

    Refactoring index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,9 @@
-function Hello($scope, $http) {
-    $http.get('http://localhost:8080/lt/').
-        success(function(data) {
-            $scope.lt = data;
-        });
-}
+    var app = angular.module('HomeScreen', []);
+
+
+    app.controller('SimpleCtrl', function($scope) {
+        $scope.title = "Susisiekite su mumis";
+        $scope.register = "Registruokitės konsultacijai internetu";
+        $scope.contacts = "Parašykite mums";
+        document.title = $scope.title;
+    });
