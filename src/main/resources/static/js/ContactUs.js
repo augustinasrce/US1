98235775e7a8571db1b4c7549104eb7c71901e29
kit <nn.justina@gmail.com>
2016-02-13 01:00:12 +0200

    ContactUs html ir js. Truksta surinkimo duomenu

--- src/main/resources/static/js/ContactUs.js
+++ src/main/resources/static/js/ContactUs.js
@@ -1,52 +1,18 @@
-angular.module('ContactUs', []){
+var contactUs = angular.module('ContactUs', []);
 
 
-.service('fbAuth', function($q, $firebase, $firebaseAuth, fbRef) {
-  var auth;
-  return function () {
-      if (auth) return $q.when(auth);
-      var authObj = $firebaseAuth(fbRef);
-      if (authObj.$getAuth()) {
-        return $q.when(auth = authObj.$getAuth());
-      }
-      var deferred = $q.defer();
-      authObj.$authAnonymously().then(function(authData) {
-          auth = authData;
-          deferred.resolve(authData);
-      });
-      return deferred.promise;
-  }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.submitInfo function(){
-    $http({
-      method: 'PUT',
-      url: '/ContactUs'},
-
-
-}
-
-
-$http({
-  method: 'PUT',
-  url: '/ContactUs'
-}).then(function successCallback(response) {
-
-  }, function errorCallback(response) {
-s.
-  });
+    contactUs.controller('ContactUsController', function($scope){
+        $scope.titleTag = "Parašykite mums";
+        $scope.themeTag = "Tema";
+        $scope.themeDefaultMessage = "Pasirinkite";
+        $scope.themeOptions= ["Taupymas ir investavimas","Pensijos ir kaupimas","Gyvybės draudimas","Ne gyvybės draudimas",
+                               "Paskolos, lizingas","Kasdienių finansų valdymas"];
+        $scope.responseWayChoices = ["Telefonu","El.paštu","Telefonu ir El.paštu"];
+        $scope.messageTag = "Žinutės tekstas";
+        $scope.client = {
+            name: "Vardas",
+            surname: "Pavardė",
+            phone: "Telefonas",
+            email: "El.paštas",
+            responseWay: "Kaip pageidaujate gauti atsakymą"};
+        });
