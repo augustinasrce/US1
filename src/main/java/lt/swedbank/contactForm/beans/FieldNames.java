f61d8939070a7ba94c96fe9739a312109b3bdeaf
p998jfn <p998jfn@myntet.se>
2016-02-15 14:33:56 +0200

    Added registrationLists to FieldNames

--- src/main/java/lt/swedbank/contactForm/beans/FieldNames.java
+++ src/main/java/lt/swedbank/contactForm/beans/FieldNames.java
@@ -1,6 +1,8 @@
 package lt.swedbank.contactForm.beans;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FieldNames {
     ArrayList<String> homeScreenNameList;
@@ -8,6 +10,9 @@ public class FieldNames {
     ArrayList<String> contactUsDropDownMenuList;
     ArrayList<String> contactUsRadioButtonList;
     ArrayList<String> contactUsConfirmationNameList;
+    ArrayList<String> bankDivisions;
+    ArrayList<String> topics;
+    Map<String, String> registrationNameTags;
 
     public FieldNames() {
         homeScreenNameList = new ArrayList<String>();
@@ -15,6 +20,10 @@ public class FieldNames {
         contactUsDropDownMenuList = new ArrayList<String>();
         contactUsRadioButtonList = new ArrayList<String>();
         contactUsConfirmationNameList = new ArrayList<String>();
+        bankDivisions = new ArrayList<String>();
+        topics = new ArrayList<String>();
+        registrationNameTags = new HashMap<String, String>();
+
     }
 
     public void setupLanguageLT() {
@@ -56,6 +65,31 @@ public class FieldNames {
                 "laukiame Jūsų skambučių telefonu 1884 arba tel. +370 5 268 4444, " +
                 "jei skambinate iš užsienio. Linkime geros dienos!");
         contactUsConfirmationNameList.add("< Atgal");
+
+        bankDivisions.add("Antakalnio g. 45");
+        bankDivisions.add("Ateities g. 91 (PC „Mandarinas\")");
+        bankDivisions.add("Gedimino pr. 56");
+        bankDivisions.add("Konstitucijos pr. 20A, dirba Būsto Centras");
+        bankDivisions.add("Ozo g. 25 (PPC „Akropolis\")");
+
+        topics.add("Taupymas ir investavimas");
+        topics.add("Pensijos ir kaupimas");
+        topics.add("Gyvybės draudimas");
+        topics.add("Ne gyvybės draudimas");
+        topics.add("Paskolos, lizingas");
+        topics.add("Kasdienių finansų valdymas");
+
+        registrationNameTags.put("title","Registracija");
+        registrationNameTags.put("header","Registruokitės konsultacijai internetu");
+        registrationNameTags.put("name","Vardas *");
+        registrationNameTags.put("surname","Pavardė *");
+        registrationNameTags.put("phone","Telefonas susisiekti *");
+        registrationNameTags.put("email","El.Paštas");
+        registrationNameTags.put("bankDivision","Banko skyrius *");
+        registrationNameTags.put("date","Data *");
+        registrationNameTags.put("topic","Tema *");
+        registrationNameTags.put("other","Pastabos, komentarai");
+        registrationNameTags.put("confirmButton","Siųsti");
     }
 
 
@@ -78,5 +112,11 @@ public class FieldNames {
     public ArrayList<String> getContactUsConfirmationNameList() {
         return contactUsConfirmationNameList;
     }
+
+    public ArrayList<String> getTopicList(){return topics;}
+
+    public ArrayList<String> getBankDivisionList(){return bankDivisions;}
+
+    public Map<String,String> getRegistrationNameTags(){return registrationNameTags;}
 
 }
