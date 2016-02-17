fb9002df715f87bbb5030d37221245e0c3a333ab
BN <benn1995@gmail.com>
2016-02-17 16:46:43 +0200

    Added SqlService class with method getAllRegistrations

---
+++ src/main/java/lt/swedbank/contactForm/services/SqlService.java
@@ -0,0 +1,117 @@
+package lt.swedbank.contactForm.services;
+
+import lt.swedbank.contactForm.beans.FieldNames;
+import lt.swedbank.contactForm.beans.Language;
+import lt.swedbank.contactForm.beans.SqlConnection;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+
+/**
+ * Created by BN on 2/17/2016.
+ */
+@RestController
+public class SqlService {
+
+    Language language = null;
+
+    String testas = null;
+    @PostConstruct
+    public void init() {
+    }
+
+    @RequestMapping("/sql")
+    public ArrayList<String> getALLRegistrations() {
+        SqlConnection sqlConnection = new SqlConnection();
+
+        ArrayList<String> result = new ArrayList<String>();
+        result = sqlConnection.selectRegistations("");
+
+        sqlConnection.closeConnection();
+        return result;
+    }
+
+    /*@RequestMapping("/en")
+    public FieldNames getLanguageEN() {
+        language = new Language();
+        return language.getFieldNamesLT();
+    }
+
+    /*
+    @RequestMapping("/lt/contact")
+    public FieldNames getLanguageForContactUsScreenLT() {
+        language = new Language();
+        return language.getFieldNamesForContactUsScreenLT();
+    }
+
+/*
+    @RequestMapping("lt/index.html")
+    public Language getLTLanguage() {
+        return language;
+    }
+
+
+        @RequestMapping("/peanuts/sorted")
+        public PeanutList sortedList() {
+            Collections.sort(peanutList.getPeanuts());
+            return peanutList;
+        }
+
+        @RequestMapping("/two")
+        public List<Peanut> two() {
+            List<Peanut> list = new ArrayList<Peanut>();
+            list.add(peanutList.getPeanuts().get(0));
+            list.add(peanutList.getPeanuts().get(1));
+            return list;
+        }
+
+        @RequestMapping("/peanuts/withO")
+        public List<Peanut> peanutsWithO() {
+            List<Peanut> list = new ArrayList<Peanut>();
+            for(Peanut peanut : peanutList.getPeanuts())
+            {
+                if(peanut.getName().contains("o"))
+                    list.add(peanut);
+            }
+
+            return list;
+        }
+
+
+        @RequestMapping("/peanuts/dividableby3")
+        public PeanutList dividableBy3List() {
+
+            PeanutList dividableBy3List = new PeanutList();
+            for (Peanut peanut : peanutList.getPeanuts()) {
+                if (peanut.getId() % 3 == 0)
+                    dividableBy3List.addPeanut(peanut,peanut.getId());
+            }
+            return dividableBy3List;
+        }
+
+
+        @RequestMapping(value = "/peanuts", method = RequestMethod.PUT)
+        public void create(String name) {
+            peanutList.addPeanut(new Peanut(name));
+        }
+
+
+        @RequestMapping(value = "/peanuts/{id}", method = RequestMethod.GET)
+        public Peanut getPeanut(@PathVariable Long id) {
+            for (Peanut peanut : peanutList.getPeanuts()) {
+                if (peanut.getId() == id.intValue())
+                    return peanut;
+            }
+            return null;
+        }
+
+
+        @RequestMapping(value = "/peanuts/{id}", method = RequestMethod.DELETE)
+        public void removePeanut(@PathVariable Long id) {
+
+        }
+*/
+
+}
