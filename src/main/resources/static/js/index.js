4cebb3c795ee88ea48f5568c56a0ad0e1c2bf6d7
p998ueh <augustinas.sueris@gmail.com>
2016-02-17 17:18:54 +0200

    Added Nav

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,14 +1,34 @@
+    var language = 'lt';
     var app = angular.module('HomeScreen', []);
 
 
     app.controller('HomeScreenController', function($scope, $http) {
     $http({
       method: 'GET',
-      url: '/lt'
+      url: '/'+language
     }).then(function successCallback(response) {
         $scope.title = response.data.homeScreenNameList[0];
         $scope.register = response.data.homeScreenNameList[1];
         $scope.contacts = response.data.homeScreenNameList[2];
         document.title = $scope.title;
       });
+
+
+
+          $('.nav li').on('click', function(){
+              $('.nav li').removeClass('active');
+              $(this).addClass('active');
+              checkLanguage();
+          });
+
     });
+
+
+    function checkLanguage(){
+        language = 'lt';
+        $('#lt').addClass("active");
+        $("#en").removeClass("active");
+        $("#ru").removeClass("active");
+        $("#ee").removeClass("active");
+        $("#lv").removeClass("active");
+    }
