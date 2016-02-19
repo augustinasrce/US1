48fd61c1199f26000f428212950c795e7082c05f
Augustinas <augustinas.sueris@gmail.com>
2016-02-19 21:37:25 +0200

    Fixed ContactsUS.

--- src/main/resources/static/js/contact.js
+++ src/main/resources/static/js/contact.js
@@ -28,19 +28,19 @@
                                 $scope.isEmailRequired = false;
                                 }
 
-           $http({
-             method: 'GET',
-             url: '/' + language
-           }).then(function successCallback(response) {
-               $scope.nameList = response.data.contactUsNameList;
-               $scope.dropDownListName = response.data.dropDownListName;
-               $scope.dropDownList = response.data.contactUsDropDownMenuList;
-               $scope.radioButtonList = response.data.contactUsRadioButtonList;
-               $scope.confirmationNameList = response.data.contactUsConfirmationNameList;
-               $scope.errorList = response.data.errorMessagesList;
+    $http({
+     method: 'GET',
+     url: '/' + language
+    }).then(function successCallback(response) {
+       $scope.nameList = response.data.contactUsNameList;
+       $scope.dropDownListName = response.data.dropDownListName;
+       $scope.dropDownList = response.data.contactUsDropDownMenuList;
+       $scope.radioButtonList = response.data.contactUsRadioButtonList;
+       $scope.confirmationNameList = response.data.contactUsConfirmationNameList;
+       $scope.errorList = response.data.errorMessagesList;
 
-               document.title = $scope.nameList[0];
-           });
+       document.title = $scope.nameList[0];
+    });
 
     $scope.UpdateData = function () {
         $http.put('/US1/'+$('#inputTopic').val()+'/'+$('#inputMessage').val()+'/'+$('#inputName').val()+'/'+$('#inputSurname').val()+'/'+$('#inputPhone').val()+'/'+$('#inputEmail').val()+'/'+$('#inputContactBy').val()+'/')
