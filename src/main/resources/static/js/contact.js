d16747ad1849f97ae98bf4391f5ddb7058c94649
p998ueh <augustinas.sueris@gmail.com>
2016-02-19 17:19:45 +0200

    New Database Tables

--- src/main/resources/static/js/contact.js
+++ src/main/resources/static/js/contact.js
@@ -4,37 +4,49 @@
 
     app.controller('ContactUsScreenController', function($scope, $http) {
 
-            $scope.isEmailRequired = true;
+    $scope.isEmailRequired = true;
+    $scope.isPhoneRequired = true;
 
-            $scope.hideEmailField = function(){
-                                $("#inputEmailDiv").addClass("hidden");
-                                $("#inputPhoneDiv").removeClass("hidden");
-                                $scope.isEmailRequired = false;
-                                }
+    $scope.showPhoneEmailFields = function(){
+        $("#inputPhoneDiv").removeClass("hidden");
+        $("#inputEmailDiv").removeClass("hidden");
+        $scope.isEmailRequired = true;
+        $scope.isPhoneRequired = true;
+    }
 
-           $http({
-             method: 'GET',
-             url: '/' + language
-           }).then(function successCallback(response) {
-               $scope.nameList = response.data.contactUsNameList;
-               $scope.dropDownListName = response.data.dropDownListName;
-               $scope.dropDownList = response.data.contactUsDropDownMenuList;
-               $scope.radioButtonList = response.data.contactUsRadioButtonList;
-               $scope.confirmationNameList = response.data.contactUsConfirmationNameList;
+    $scope.hidePhoneField = function(){
+        $("#inputPhoneDiv").addClass("hidden");
+        $("#inputEmailDiv").removeClass("hidden");
+        $scope.isPhoneRequired = false;
+        $scope.isEmailRequired = true;
+    }
 
-               document.title = $scope.nameList[0];
-           });
+    $scope.hideEmailField = function(){
+        $("#inputEmailDiv").addClass("hidden");
+        $("#inputPhoneDiv").removeClass("hidden");
+        $scope.isEmailRequired = false;
+        $scope.isPhoneRequired = true;
+    }
 
-     $scope.UpdateData = function () {
-           $http.put('/US1/'+$('#inputTopic').val()+'/'+$('#inputMessage').val()+'/'+$('#inputName').val()+'/'+$('#inputSurname').val()+'/'+$('#inputPhone').val()+'/'+$('#inputEmail').val()+'/'+$('#inputContactBy').val()+'/')
-           .success(function (data, status, headers) {
-                $scope.ServerResponse = data;
-            })
-        };
-
-
-
-
+    $http({
+    method: 'GET',
+    url: '/' + language
+    }).then(function successCallback(response) {
+        $scope.nameList = response.data.contactUsNameList;
+        $scope.dropDownListName = response.data.dropDownListName;
+        $scope.dropDownList = response.data.contactUsDropDownMenuList;
+        $scope.radioButtonList = response.data.contactUsRadioButtonList;
+        $scope.confirmationNameList = response.data.contactUsConfirmationNameList;
+        document.title = $scope.nameList[0];
     });
+
+    $scope.UpdateData = function () {
+        $http.put('/US1/'+$('#inputTopic').val()+'/'+$('#inputMessage').val()+'/'+$('#inputName').val()+'/'+$('#inputSurname').val()+'/'+$('#inputPhone').val()+'/'+$('#inputEmail').val()+'/'+$('#inputContactBy').val()+'/')
+        .success(function (data, status, headers) {
+            $("#formDiv").addClass("hidden");
+            $("#confirmDiv").removeClass("hidden");
+        })
+    };
+});
 
 
